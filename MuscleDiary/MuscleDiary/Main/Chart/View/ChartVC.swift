84b3cf7bffabd3e778564aa10905b1c6d448f13a
Yoobin JO <dbqlsdl1234@naver.com>
2020-05-01 19:42:18 +0900

    Chart, Exercise 도메인 생성 및 ViewController 파일 생성

---
+++ MuscleDiary/MuscleDiary/Main/Chart/View/ChartVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChartVC.swift
+//  MuscleDiary
+//
+//  Created by YooBin Jo on 2020/05/02.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class ChartVC: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
