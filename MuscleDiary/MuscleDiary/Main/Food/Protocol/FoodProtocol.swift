7d8f91866da3e80f7f3ff233e7f40f9793eaff27
Yoobin JO <dbqlsdl1234@naver.com>
2020-06-06 14:53:52 +0900

    음식 관련 도메인에 적용할 프로토콜 생성

---
+++ MuscleDiary/MuscleDiary/Main/Food/Protocol/FoodProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  FoodProtocol.swift
+//  MuscleDiary
+//
+//  Created by YooBin Jo on 2020/06/01.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+protocol FoodProtocol {
+    var recommendCarbohydrate: Double { get set }
+    
+    var recommendProtein: Double { get set }
+    
+    var recommendFat: Double { get set }
+    
+}
